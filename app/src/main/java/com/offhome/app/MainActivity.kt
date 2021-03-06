73c841dee1bc08d574433b93c9e8c296e95f8877
Pau Cuesta <pau.cuesta@gmail.com>
2021-03-06 19:05:32 +0100

    Correcció integració ktlint i afegit Dokka

--- app/src/main/java/com/offhome/app/MainActivity.kt
+++ app/src/main/java/com/offhome/app/MainActivity.kt
@@ -3,7 +3,13 @@ package com.offhome.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Main class
+ */
 class MainActivity : AppCompatActivity() {
+    /**
+     * oncreate App
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

24211bd708ec03a0a4a5bf5c15bfebd0e6a52057
Pau Cuesta <pau.cuesta@gmail.com>
2021-03-07 16:55:33 +0100

    Afegit Binding

--- app/src/main/java/com/offhome/app/MainActivity.kt
+++ app/src/main/java/com/offhome/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.offhome.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.offhome.app.databinding.ActivityMainBinding
 
 /**
  * Main class
@@ -10,8 +11,12 @@ class MainActivity : AppCompatActivity() {
     /**
      * oncreate App
      */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textViewHelloWorld.text = "OFF Home"
     }
 }

be8157d945cdd85db61c8c127b77efd8e7059aab
Ferran <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-03-22 18:42:58 +0100

    [Pantalla Profile] fent ProfileMyActivitiesFragment
    recyclerView, cardView
    
    TG-104

--- app/src/main/java/com/offhome/app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/offhome/app/ui/profile/ProfileFragment.kt
@@ -1,11 +1,11 @@
 package com.offhome.app.ui.profile
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.offhome.app.R
 
 class ProfileFragment : Fragment() {
@@ -17,19 +17,19 @@ class ProfileFragment : Fragment() {
     private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //treure?
-        //viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        // treure?
+        // viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         return inflater.inflate(R.layout.profile_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)     //descomentar?
+        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java) // descomentar?
         // TODO: Use the ViewModel
     }
-
 }

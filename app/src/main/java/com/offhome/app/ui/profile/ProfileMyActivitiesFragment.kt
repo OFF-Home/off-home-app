8df9958fdd025ab068ed8820c7b99a5b526e296b
Ferran <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-03-22 18:07:30 +0100

    [Pantalla Profile] afegit ProfileMyActivitiesFragment2
    i ficat una cosa de scroll a ProfileFragment
    
    TG-104

--- app/src/main/java/com/offhome/app/ui/profile/ProfileMyActivitiesFragment.kt
+++ app/src/main/java/com/offhome/app/ui/profile/ProfileMyActivitiesFragment.kt
@@ -1,11 +1,11 @@
 package com.offhome.app.ui.profile
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.offhome.app.R
 
 class ProfileMyActivitiesFragment : Fragment() {
@@ -17,7 +17,8 @@ class ProfileMyActivitiesFragment : Fragment() {
     private lateinit var viewModel: ProfileMyActivitiesViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.profile_my_activities_fragment, container, false)
@@ -28,5 +29,4 @@ class ProfileMyActivitiesFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ProfileMyActivitiesViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }

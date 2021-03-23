d547388725d0535384f1105e7b341b283a3ade82
emma <emmapereira23@gmail.com>
2021-03-23 23:42:44 +0100

    [Seleccionar activitat] Més canvis pantalles + botó back

--- app/src/main/java/com/offhome/app/ui/infoactivity/InfoActivity.kt
+++ app/src/main/java/com/offhome/app/ui/infoactivity/InfoActivity.kt
@@ -17,6 +17,9 @@ class InfoActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment

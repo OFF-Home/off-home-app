239abeafbfa2fd4d1c2881b6fb214c228dc238dd
Ferran <ferran.iglesias.barenys@estudiantat.upc.edu>
2021-03-23 09:58:54 +0100

    [Profile] fent el Profile Fragment
    TG-4

--- app/src/main/java/com/offhome/app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/offhome/app/ui/profile/ProfileFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.offhome.app.R
@@ -15,6 +16,7 @@ class ProfileFragment : Fragment() {
     }*/
 
     private lateinit var viewModel: ProfileViewModel
+    lateinit var imageViewProfilePic:ImageView// = findViewById<ImageView>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,13 +25,18 @@ class ProfileFragment : Fragment() {
     ): View? {
         // treure?
         // viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
+        val view = inflater.inflate(R.layout.profile_fragment, container, false)
 
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+        /*imageViewProfilePic = view.findViewById(R.id.imageViewProfilePic)
+        imageViewProfilePic.setImageDrawable( "@drawable/profile_pic_placeholder")*/
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java) // descomentar?
         // TODO: Use the ViewModel
+
     }
 }

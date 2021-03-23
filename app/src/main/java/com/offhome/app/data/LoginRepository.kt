887c42b36d0b32e69a5828331dc7818862b0bc05
Pau Cuesta <pau.cuesta@gmail.com>
2021-03-23 23:42:57 +0100

    [Login] LoginRepository.kt documented

--- app/src/main/java/com/offhome/app/data/LoginRepository.kt
+++ app/src/main/java/com/offhome/app/data/LoginRepository.kt
@@ -6,6 +6,10 @@ import com.offhome.app.data.model.LoggedInUser
 /**
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
+ * @author Pau Cuesta Arcos
+ * @param dataSource references the Data Source from where to get de data
+ * @property user is the user that has logged in
+ * @property isLoggedIn has constancy if there is a logged in user or not
  */
 
 class LoginRepository(val dataSource: LoginDataSource) {
@@ -20,13 +24,22 @@ class LoginRepository(val dataSource: LoginDataSource) {
         user = null
     }
 
+    /**
+     * Calls to the data source to log out and deletes the info of the user
+     */
     fun logout() {
         user = null
         dataSource.logout()
     }
 
-    fun login(username: String, password: String): Result<LiveData<LoggedInUser>> {
-        val result = dataSource.login(username, password)
+    /**
+     * Calls to the data source to log in and if successful, saves de result into de loggedInUser
+     * @param email is the email of the user
+     * @param password is the password of the user
+     * @return the result with a live data of the log in
+     */
+    fun login(email: String, password: String): Result<LiveData<LoggedInUser>> {
+        val result = dataSource.login(email, password)
 
         if (result is Result.Success) {
             setLoggedInUser(result.data.value)
@@ -35,6 +48,9 @@ class LoginRepository(val dataSource: LoginDataSource) {
         return result
     }
 
+    /**
+     * Sets the logged in user
+     */
     private fun setLoggedInUser(loggedInUser: LoggedInUser?) {
         this.user = loggedInUser
     }

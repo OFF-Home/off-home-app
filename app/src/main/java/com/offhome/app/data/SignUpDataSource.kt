3727220143e8faed1ca07f8146d6535ea2d6ae51
Pau Cuesta <pau.cuesta@gmail.com>
2021-03-24 18:00:45 +0100

    [SignUp] Connexió backend
    Carry al Ferran.
    
    #TG-4

--- app/src/main/java/com/offhome/app/data/SignUpDataSource.kt
+++ app/src/main/java/com/offhome/app/data/SignUpDataSource.kt
@@ -1,13 +1,18 @@
 package com.offhome.app.data
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.offhome.app.data.model.SignedUpUser
 import com.offhome.app.data.retrofit.SignUpService
 import com.offhome.app.ui.signup.SignUpActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
@@ -22,15 +27,16 @@ class SignUpDataSource {
 
     private var _result = MutableLiveData<Result>()
     val result: LiveData<Result> = _result // aquest es observat per Repository
-    private lateinit var retrofit: Retrofit
-    private lateinit var signUpService: SignUpService
+    private var retrofit: Retrofit =
+        Retrofit.Builder().baseUrl("http://ec2-100-25-149-77.compute-1.amazonaws.com:3000/").addConverterFactory(GsonConverterFactory.create()).build()
+    private var signUpService: SignUpService = retrofit.create(SignUpService::class.java)
 
     init {
         // per parlar amb el nostre server
         // aixo crea el Client
-        retrofit = Retrofit.Builder().baseUrl("http://ec2-100-25-149-77.compute-1.amazonaws.com:3000/users/").addConverterFactory(GsonConverterFactory.create()).build() // TODO la adreça potser no esta be
+        // TODO la adreça potser no esta be
         // aixo retorna una implementacio de la interface
-        signUpService = retrofit.create(SignUpService::class.java) // lo de ::class ho ha fet el Pau aixi
+        // lo de ::class ho ha fet el Pau aixi
     }
 
     fun signUp(email: String, username: String, password: String, birthDate: Date, activity: SignUpActivity) { // TODO treure activity quan acabi de debugejar
@@ -49,28 +55,37 @@ class SignUpDataSource {
                     }
 
                     _result.value = Result(success = true) // Result.Success(true)
-/*
                     // parlar amb el nostre client
-                    val call: Call<String> = signUpService.createProfile(username)
-                    val response = call.execute() // we actually execute the call. I agafo el body perque potser conte la resposta xd
-                    // response.body()
-                    if (response.isSuccessful) {
-                        Toast.makeText(
-                            activity,
-                            // "$emailConfirmationMessage $displayName",
-                            "HTTP call success",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    } else {
-                        Toast.makeText(
-                            activity,
-                            // "$emailConfirmationMessage $displayName",
-                            "HTTP call error",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
+                    val signedUpUser = SignedUpUser(email, username, password, birthDate.toString())
+                    val call: Call<String> = signUpService.createProfile(username, signedUpUser)
+                    call.enqueue(object: Callback<String> {
+                        override fun onResponse(call: Call<String>, response: Response<String>) {
+                            if (response.isSuccessful) {
+                                Toast.makeText(
+                                    activity,
+                                    // "$emailConfirmationMessage $displayName",
+                                    "HTTP call success",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            } else {
+                                Toast.makeText(
+                                    activity,
+                                    // "$emailConfirmationMessage $displayName",
+                                    "HTTP call error",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
 
- */
+                        override fun onFailure(call: Call<String>, t: Throwable) {
+                            Toast.makeText(
+                                activity,
+                                "Connection error",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+
+                    })
                 } else {
                     Log.w("Sign-up", "createUserWithEmail:failure", task.exception)
 

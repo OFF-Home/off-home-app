2ea5ceee7e380d0c7601d5c13334387ab37a2fb5
maria <marianievas11@gmail.com>
2021-03-24 17:32:27 +0100

    [Crear Activitat] últim commit

---
+++ app/src/main/java/com/offhome/app/ui/createactivity/CreateActivityUtil.kt
@@ -0,0 +1,23 @@
+package com.offhome.app.ui.createactivity
+
+object CreateActivityUtil {
+/*
+    private val existingActivity = listOf("Running in La Barceloneta")
+
+    /**
+     * the input is not valid if ...
+     *      the title/description/date/location is empty
+     **/
+
+    fun validationCreateActivityInput(
+        title: String,
+        description: String,
+        date: String,
+        maximumParticipants: Int,
+        location: String,
+        category: String,
+    ): Boolean {
+        return true
+    }
+*/
+}

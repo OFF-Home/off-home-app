c83cbaf0e2ff2d767952c12685e1a9039e1c7120
emma <emmapereira23@gmail.com>
2021-03-25 19:54:56 +0100

    [Seleccionar categories] només faig commit per cambiar la branca

--- app/src/main/java/com/offhome/app/ui/activitieslist/ActivitiesListRecyclerViewAdapter.kt
+++ app/src/main/java/com/offhome/app/ui/activitieslist/ActivitiesListRecyclerViewAdapter.kt
@@ -1,10 +1,12 @@
 package com.offhome.app.ui.activitieslist
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.offhome.app.R
@@ -46,6 +48,13 @@ class  ActivitiesListRecyclerViewAdapter : RecyclerView.Adapter<ActivitiesListRe
                 Glide.with(holder.mView.context).load(R.drawable.ic_baseline_favorite_border_24).centerCrop().into(holder.iconLikeImage)
             }
         }
+
+        /* al clicar a la activitat pasar com a parametre el nom i la hora????????
+        holder.mView.setOnClickListener() {
+            val intent = Intent(this, ActivitiesList::class.java);
+            intent.putExtra("amount", "Running")
+            startActivity(intent)
+        }*/
     }
 
     override fun getItemCount(): Int = activitiesList.size

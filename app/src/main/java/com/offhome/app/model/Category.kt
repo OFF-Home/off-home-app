2ed35e80c7b394f85f39799a15c893ed1e1b8fcf
Pau Cuesta <pau.cuesta@gmail.com>
2021-03-25 08:48:21 +0100

    [Mostrar Categories] Canvi model
    #TG-54

--- app/src/main/java/com/offhome/app/model/Category.kt
+++ app/src/main/java/com/offhome/app/model/Category.kt
@@ -3,8 +3,6 @@ package com.offhome.app.model
 import java.util.*
 
 data class Category(
-    val nom: String,
-    val description: String,
-    val createdAt: String,
-    val updatedAt: String
+    val categoria: String,
+    val descripcio: String,
 )

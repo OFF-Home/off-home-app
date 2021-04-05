7750956c5247df32614ae0c34a395c0dc8296ac2
emma <emmapereira23@gmail.com>
2021-04-05 20:58:19 +0200

    [Interaccionar amb el mapa] Nova activity amb mapa y posicionar-lo amb zooms

---
+++ app/src/main/java/com/offhome/app/ui/map/MapWithActivities.kt
@@ -0,0 +1,45 @@
+package com.offhome.app.ui.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.offhome.app.R
+
+class MapWithActivities : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.setZoomControlsEnabled(true)
+
+        // Add a marker in Sydney and move the camera
+        val barcelona = LatLng(41.3879, 2.16992)
+        //mMap.addMarker(MarkerOptions().position(barcelona).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(barcelona, 13.0f))
+    }
+}

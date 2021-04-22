8ffac5c5386146ce069f4f1f07c1dacc6967261d
maria <marianievas11@gmail.com>
2021-04-22 20:25:01 +0200

    [SearchActivity] Funcionen tots els filtres menys els de categoria

--- app/src/main/java/com/offhome/app/ui/activitieslist/ActivitiesListFragment.kt
+++ app/src/main/java/com/offhome/app/ui/activitieslist/ActivitiesListFragment.kt
@@ -3,7 +3,9 @@ package com.offhome.app.ui.activitieslist
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.ArrayAdapter
 import android.widget.SearchView
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.offhome.app.R
 import com.offhome.app.model.ActivityFromList
+
 
 class ActivitiesListFragment : Fragment() {
 
@@ -22,6 +25,9 @@ class ActivitiesListFragment : Fragment() {
     private lateinit var activitiesViewModel: ActivitiesViewModel
     private lateinit var activitiesListAdapter: ActivitiesListRecyclerViewAdapter
     private var activitiesList: List<ActivityFromList> = ArrayList()
+
+    private val spinnerDialog = view?.findViewById<Spinner>(R.id.spinnerCategories)
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,7 +64,7 @@ class ActivitiesListFragment : Fragment() {
         val menuItem = menu.findItem(R.id.search)
         val searchView = menuItem.actionView as SearchView
         searchView.maxWidth = Int.MAX_VALUE
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
@@ -71,32 +77,57 @@ class ActivitiesListFragment : Fragment() {
     }
 
     //per fer el sort
-            //sort ascending/descending , order by data, seleccionar quina categoria es vol
+    //sort ascending/descending , order by data, seleccionar quina categoria es vol
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-      /*  when (item.itemId) {
+        /*  when (item.itemId) {
             R.id.activity_title -> sortActivities()
         }*/
         val id = item.itemId
-        if (id == R.id.action_sort){
+        if (id == R.id.action_sort) {
             sortActivities()
+        }
+        else if (id == R.id.action_sort_categories){
+            sortActivitiesByCategory()
         }
         return super.onOptionsItemSelected(item)
     }
 
     private fun sortActivities() {
-
         val dialog = AlertDialog.Builder(context)
-        dialog.setTitle("Sort").setItems(arrayOf("Ascending", "Descending", "By date", "By category")){
-                dialogInterface, i ->
-            if (i==0){
-                //ascending clicked
-                dialogInterface.dismiss()
-            }
-            else {
-                dialogInterface.dismiss()
-                //descending clicked
-            }
-        }.show()
+        dialog.setTitle("Sort")
+            .setItems(arrayOf("Ascending", "Descending", "By date")) { dialogInterface, i ->
+                if (i == 0) {
+                    //ascending clicked
+                    dialogInterface.dismiss()
+                    activitiesList.sortedBy { it.titol }
+                } else if (i == 1) {
+                    //descending clicked
+                    dialogInterface.dismiss()
+                    activitiesList.sortedByDescending { it.titol }
+                } else if (i == 2) {
+                    //sorted by date
+                    dialogInterface.dismiss()
+                    activitiesList.sortedBy { it.dataHoraIni }
+                }
+            }.show()
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(ActivitiesListFragment.this,android.R.layout.spinnerDialog);
+
     }
 
+    private fun sortActivitiesByCategory(){
+        val dialog2 = AlertDialog.Builder(context)
+        dialog2.setTitle("Sort by category").setItems(spinnerDialog?.resources?.getStringArray(R.array.select_category)) { dialogInterface, i ->
+            dialogInterface.dismiss()
+        }.show()
+
+       /* val adapter = ArrayAdapter.createFromResource(this, R.array.select_category, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerDialog.setAdapter(adapter)
+        spinnerDialog.setOnItemSelectedListener()*/
+    }
 }
+       /* dialog.setItems("select_category", DialogInterface.OnClickListener { dialogInterface, item -> // Do something with the selection
+            dialogInterface.dismiss()
+            Toast.makeText((ActivitiesListFragment.this,select_aa))
+            })    }*/
+

37fce7d188d3f2503c8ba4998f975e20abde5a48
maria <marianievas11@gmail.com>
2021-04-23 18:36:26 +0200

    [updatePassword] errors solucionats

--- app/src/main/java/com/offhome/app/ui/updatePassword/UpdatePasswordActivity.kt
+++ app/src/main/java/com/offhome/app/ui/updatePassword/UpdatePasswordActivity.kt
@@ -1,29 +1,32 @@
 package com.offhome.app.ui.updatePassword
 
-import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.LayoutInflater
+import android.text.method.PasswordTransformationMethod
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.offhome.app.R
-import com.offhome.app.databinding.ActivitiesActivityBinding.inflate
-import com.offhome.app.databinding.ActivityActivitiesListBinding.inflate
-import com.offhome.app.ui.profile.ProfileFragmentViewModel
+import com.offhome.app.ui.login.LoginActivity
+
 
 class UpdatePasswordActivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var btnAcceptChange : Button
     private lateinit var text : TextView
     private lateinit var firstText : EditText
     private lateinit var secondText : EditText
-
+    private lateinit var thirdText : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,30 +36,78 @@ class UpdatePasswordActivity: AppCompatActivity() {
         setLayout()
 
         btnAcceptChange.setOnClickListener {
-
+            changePassword()
+            resetLayout()
         }
     }
 
     private fun setLayout(){
-        secondText = findViewById(R.id.editTextNew)
-        secondText.visibility = VISIBLE;
-        firstText = findViewById(R.id.editTextEmailToRecover)
         text = findViewById(R.id.textView)
         text.visibility = INVISIBLE;
+        firstText = findViewById(R.id.editTextEmailToRecover)
+        firstText.hint = "current password"
+        firstText.transformationMethod = PasswordTransformationMethod()
+        secondText = findViewById(R.id.editTextNew)
+        secondText.visibility = VISIBLE;
+        secondText.transformationMethod = PasswordTransformationMethod()
+        thirdText = findViewById((R.id.editTextNewRepeat))
+        thirdText.visibility = VISIBLE
+        thirdText.transformationMethod = PasswordTransformationMethod()
         btnAcceptChange = findViewById(R.id.buttonRecoverPassword)
-        firstText.hint = "Current password"
         btnAcceptChange.text = "Change Password"
     }
-}
 
+    private fun resetLayout(){
+        text.visibility = VISIBLE;
+        firstText.transformationMethod = null
+        secondText.visibility = INVISIBLE;
+        thirdText.visibility = INVISIBLE
+    }
 
-    /*
-    fragmentViewModel = ViewModelProvider(this).get(ProfileFragmentViewModel::class.java)
-    fragmentViewModel.getProfileInfo()
-    fragmentViewModel.profileInfo.observe(
-        viewLifecycleOwner,
-        Observer { // aquest observer no arriba a executar-se però el de AboutMeFragment sí. NO ENTENC PERQUÈ
-            val profileInfoVM = it ?: return@Observer
-            username = profileInfoVM.username
+    private fun changePassword(){
+        if (firstText.text.isNotEmpty() && secondText.text.isNotEmpty() && thirdText.text.isNotEmpty()){
+            if (secondText.text.toString() == thirdText.text.toString()){
+                val myUser = auth.currentUser
+                if (myUser != null && myUser.email != null){
+                    val myUser = Firebase.auth.currentUser!!
+
+                    // Get auth credentials from the user for re-authentication. The example below shows
+                    // email and password credentials but there are multiple possible providers,
+                    // such as GoogleAuthProvider or FacebookAuthProvider.
+                    val credential = EmailAuthProvider.getCredential(
+                        myUser.email!!,
+                        firstText.text.toString()
+                    )
+
+                    // Prompt the user to re-provide their sign-in credentials
+                    myUser.reauthenticate(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            Toast.makeText(this, "Re-Authentication success", Toast.LENGTH_SHORT).show()
+                            val myUser = Firebase.auth.currentUser
+
+                            myUser!!.updatePassword(secondText.text.toString())
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(
+                                            this,
+                                            "Password changed successfully",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                       // startActivity(Intent(this,MainActivity::class.java))
+                                        //finish()
+                                    }
+                                }
+                        }
+                        else Toast.makeText(this, "Re-Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }
+            }
+            else Toast.makeText(this, "Password mismatching", Toast.LENGTH_SHORT).show()
         }
-    )*/
+        else Toast.makeText(this, "Please enter all the fields", Toast.LENGTH_SHORT).show()
+    }
+}

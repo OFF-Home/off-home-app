94496e4da05985ed6b859266b55b8fa05fae2b57
maria <marianievas11@gmail.com>
2021-05-07 13:14:27 +0200

    [GroupChat] get missatges Xat

--- app/src/main/java/com/offhome/app/ui/chats/groupChat/GroupChatActivity.kt
+++ app/src/main/java/com/offhome/app/ui/chats/groupChat/GroupChatActivity.kt
@@ -1,26 +1,65 @@
 package com.offhome.app.ui.chats.groupChat
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.offhome.app.R
 import com.offhome.app.ui.chats.MyChatRecyclerViewAdapter
 
 class GroupChatActivity: AppCompatActivity() {
     private lateinit var messagesAdapter: MyChatRecyclerViewAdapter
-    private lateinit var viewModel: GroupChatActivity
+    private lateinit var viewModel: GroupChatViewModel
+
+    private lateinit var messagesList: RecyclerView
+    private lateinit var editTextNewMessage: EditText
+    private lateinit var btnSendMessage: ImageButton
+
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_chat)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //val nameAct = intent.getParcelableExtra<Activity>()
         val arguments = intent.extras
         title = arguments?.getString("This is the title of the activity")
+        val user_id = "101"
+        val date_ini = "26-5-2000 18:00"
 
-        // val intent: Intent()
-       // groupId = intent.getStringExtra()
+        messagesList = findViewById(R.id.messages_view)
+        editTextNewMessage = findViewById(R.id.new_message)
+        btnSendMessage = findViewById(R.id.sendButton)
 
+        messagesAdapter = MyChatRecyclerViewAdapter()
+        with(messagesList) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = messagesAdapter
+        }
+
+        viewModel = ViewModelProvider(this).get(GroupChatViewModel::class.java)
+
+        viewModel.listMessages.observe(
+            this,
+            {
+                messagesAdapter.setData(it)
+            }
+        )
+        user_id.let {
+            viewModel.getMessages(it,date_ini)
+        }
+
+        btnSendMessage.setOnClickListener {
+            if (editTextNewMessage.text.isEmpty())
+                Toast.makeText(
+                    this, getString(R.string.error_empty_message),
+                    Toast.LENGTH_LONG
+                ).show()
+            else viewModel.sendMessage(editTextNewMessage.text.toString())
+        }
     }
+
 }
--- app/src/main/java/com/offhome/app/ui/chats/groupChat/GroupChatViewModel.kt
+++ app/src/main/java/com/offhome/app/ui/chats/groupChat/GroupChatViewModel.kt
@@ -2,21 +2,22 @@ package com.offhome.app.ui.chats.groupChat
 
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.offhome.app.R
 import com.offhome.app.common.MyApp
 import com.offhome.app.data.ChatRepository
 import com.offhome.app.data.Result
 import com.offhome.app.model.Message
 
-class GroupChatViewModel {
+class GroupChatViewModel: ViewModel() {
     private var repository = ChatRepository()
     var listMessages: MutableLiveData<List<Message>> = MutableLiveData<List<Message>>()
 
     /**
-     * It calls the repository to get the messages of a chat
+     * It calls the repository to get the messages of a group chat
      */
     fun getMessages(uid_creator: String, data_hora_ini: String) {
-        val result = repository.getMessages(uid_creator, data_hora_ini)
+        val result = repository.getMessagesGroup(uid_creator, data_hora_ini)
         if (result is Result.Success) {
             listMessages = result.data as MutableLiveData<List<Message>>
         } else {

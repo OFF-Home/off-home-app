07503ce98d8ee7afb65b89a36c6327d3f47adb06
maria <marianievas11@gmail.com>
2021-05-13 16:02:18 +0200

    [GroupChat]firebase enviar rebre missatges

--- app/src/main/java/com/offhome/app/data/retrofit/ChatsService.kt
+++ app/src/main/java/com/offhome/app/data/retrofit/ChatsService.kt
@@ -3,20 +3,22 @@ package com.offhome.app.data.retrofit
 
 
 import com.offhome.app.data.model.ChatGroupIdentification
-import com.offhome.app.data.model.ChatIndividualIdentification
+import com.offhome.app.data.model.SendMessage
 import com.offhome.app.model.GroupMessage
 import com.offhome.app.model.Message
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.HTTP
+import retrofit2.http.*
 
 interface ChatsService {
     /**
      * This is the call for getting the messages of a individual chat
      */
-    @HTTP(method = "GET", path = "xats/individual", hasBody = true)
-    fun getAllMessages(@Body chat: ChatIndividualIdentification?): Call<List<Message>>
+    @GET("xats/individual")
+    fun getAllMessages(@Query("usid_1") usid1: String, @Query("usid_2") usid2: String): Call<List<Message>>
+
+    @POST("xats/missatgesIndividual")
+    fun sendMessage(@Body message: SendMessage): Call<ResponseBody>
 
     /**
      * This is the call for getting the messages of a group chat
@@ -24,12 +26,8 @@ interface ChatsService {
     @HTTP(method = "GET", path = "xats/grupal", hasBody = true)
     fun getAllMessagesGroup(@Body chat: ChatGroupIdentification?): Call<List<Message>>
 
-    /**
-     * This is the call for sending messages of a group chat
-     */
     @HTTP(method = "POST", path = "xats/missatgesGrup", hasBody = true)
     fun sendGroupMissage(@Body chat: GroupMessage?): Call<ResponseBody>
-
     /**
      * This is the call for creating a new group chat
      */

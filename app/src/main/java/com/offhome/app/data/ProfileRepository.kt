7ff053c46ee56cf874bba747fa1bde4131d878cf
Pau Cuesta <pau.cuesta@gmail.com>
2021-06-03 03:29:20 +0200

    No salta users

--- app/src/main/java/com/offhome/app/data/ProfileRepository.kt
+++ app/src/main/java/com/offhome/app/data/ProfileRepository.kt
@@ -359,25 +359,26 @@ class ProfileRepository {
      * obtains profile info of a user from a username
      */
     fun getProfileInfoByUsername(newText: String): MutableLiveData<Result<UserInfo>> {
+        val result = MutableLiveData<Result<UserInfo>>()
         val call: Call<UserInfo> = userService!!.getProfileInfoByUsername(newText)
         call.enqueue(object : Callback<UserInfo> {
             override fun onResponse(call: Call<UserInfo>, response: Response<UserInfo>) {
                 if (response.isSuccessful) {
-                    userInfo.value = Result.Success(response.body() as UserInfo)
+                    result.value = Result.Success(response.body() as UserInfo)
                     Log.d("success response", "got a response indicating success")
                 } else {
-                    userInfo.value = Result.Error(IOException("Got a response indicating failure"))
+                    result.value = Result.Error(IOException("Got a response indicating failure"))
                     Log.d("failure response", "got a response indicating failure")
                 }
             }
 
             override fun onFailure(call: Call<UserInfo>, t: Throwable) {
-                userInfo.value = Result.Error(IOException("Error getting topProfileInfo. communication failure (no response)"))
+                result.value = Result.Error(IOException("Error getting topProfileInfo. communication failure (no response)"))
                 Log.d("GET", "Error getting topProfileInfo. communication failure (no response)")
             }
         })
 
-        return userInfo
+        return result
     }
 
     /**
